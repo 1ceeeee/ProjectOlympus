147e795ae0b2fa8b57a167da1c3001724e17d0ca
içe <lorraine.soriano02@gmail.com>
2026-07-21 02:09:19 +1200

    Create EnemyHealth.cs

---
+++ UnityProject/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log(gameObject.name + " took " + damage + " damage.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log(gameObject.name + " has been defeated.");
+
+        Destroy(gameObject);
+    }
+}
