01b2b9245bf8574ac7512aadbb954a0f80fbb6b4
içe <lorraine.soriano02@gmail.com>
2026-07-20 16:24:42 +1200

    Create PlayerController.cs

---
+++ UnityProject/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        rb.MovePosition(
+            transform.position + movement * moveSpeed * Time.deltaTime
+        );
+    }
+}
